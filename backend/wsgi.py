99324121b3fe2f85f3cd43d014aeddad9d4dd455
ceitec itapipoca <culturamakerprogramacaoeia@gmail.com>
2026-02-18 22:46:17 -0300

    Fix_wsgi_path

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Adiciona o diretório do projeto ao path
-path = '/home/username/ITAGAME/backend' # O PythonAnywhere vai precisar que você mude 'username'
+path = '/home/itagame/ITAGAME/backend' # O PythonAnywhere vai precisar que você mude 'username'
 if path not in sys.path:
     sys.path.append(path)
 
